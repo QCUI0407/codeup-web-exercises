fb0200235b1612ffbd1a5ed7adf9d63de51ddf05
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2022-10-05 14:57:30 -0500

    new JS

---
+++ JS/intro-js.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let greeting = "Hello from JS file";
+console.log(greeting);
+
+alert("You've been hacked!");
+let userName = prompt("What is your name?");
+//log the result of the prompt
+console.log(userName);
+
+let isFull = confirm("is the class full?");
+console.log(isFull);
+
+
---
+++ class exerciess.js
@@ -0,0 +1,38 @@
+Data Types
+// // boolean
+// // boolean
+// // number
+// // String
+// // undefined
+// // null
+// // number
+Basic Math Operators
+// 2
+// 2
+// 300
+// 4
+// 12
+// infinity
+// infinity
+// 0.3
+// NaN
+// NaN
+// NaNBread
+// NaNBATMAN!
+The Not Boolean Operator
+false
+true
+true
+false
+false
+false
+false
+true
+true
+false
+false
+true
+true
+false
+false
+!null
