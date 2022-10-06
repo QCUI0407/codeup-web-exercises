42353bbf6193bfe9760e5ffde8cdbf486f6ad265
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2022-10-06 14:28:46 -0500

    new functions exerciess

---
+++ class_exerciess/class_exerciess.js
@@ -0,0 +1,184 @@
+// Data Types
+// // // boolean
+// // // boolean
+// // // number
+// // // String
+// // // undefined
+// // // null
+// // // number
+// Basic Math Operators
+// // 2
+// // 2
+// // 300
+// // 4
+// // 12
+// // infinity
+// // infinity
+// // 0.3
+// // NaN
+// // NaN
+// // NaNBread
+// // NaNBATMAN!
+// The Not Boolean Operator
+// false
+// true
+// true
+// false
+// false
+// false
+// false
+// true
+// true
+// false
+// false
+// true
+// true
+// false
+// false
+// true
+// false
+// true
+// false
+// false
+// true
+// Boolean Operators
+// true
+// false
+// false
+// false
+// false
+// false
+// true
+// true
+// true
+// false
+// true
+// true
+// true
+// false
+// Typeof Operator
+// boolean
+// boolean
+// boolean
+// number
+// number
+// string
+// string
+// number
+// string3
+// NaN
+// number
+// string
+// false
+// boolean
+// boolean
+// string
+// string
+// string
+// number
+// string
+// Comparisson Operators
+// 99 === 99 //true
+// 2 === 2.0//true
+// 2 == "2"//true
+// 2 === "2"//false
+// 2 === 2.0//true
+// 4 !== 2//true
+// true === true //true
+// false === false //true
+// true === false //false
+// false === true//false
+// typeof (true === true) //bollean
+// typeof true === "boolean" //true
+// typeof false === "boolean" //
+// typeof 23 === "number"
+// typeof "banana" === "string"
+// (11 - 3) === 7
+// "Hello" === "Hello"
+// typeof "Howdy" === typeof "Howdy"
+// 5 === "5"
+// 5 == "5"
+// 5 !== "5"
+// 5 != "5"
+// 10 > 2
+// "23" > 42
+// 20 >= 20
+// 5 < 10
+// "Hello" != "World"
+// undefined == null
+// undefined === null
+// "" == false
+// "" === false
+// typeof "banana"
+// typeof true === tyepof "banana"
+// true === 1
+// true == 1
+// false === 0
+// false == 0
+// 5 === 5
+// 5 == 5
+// 5 = 5
+// 2 != "2"
+// 2 !== "2"
+// 2 != 2
+// 2 !== 2
+// undefined == false
+// undefined === false
+// null == false
+// null === false
+// 0 == false
+// 0 === false
+// "" == false
+// "" === false
+// "" == 0
+// "" === 0
+// "code" == 1
+// "code" === 1
+// "banana" == true
+// "banana" === true
+// "banana" != true
+// "banana" !== true
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// ================ !! Mini-exercises !!
+
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+
+function returnFive(a){
+    return 5
+}
+
+returnFive(a);
+
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+
+function isFive(num) {
+   let five = Number(num) == 5
+    return console.log(Boolean(five))
+}
+isFive(6)
+// how many inputs should the function have?
+// what are the data types for the inputs?
+// what is the data type of the output/return value?
+
+
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+
+// how many inputs?
+// what data type for inputs?
+// what is the return type?
+
+
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+
+// How many inputs?
+// What type of input?
+// What type of output?
--- js/functions-lec.js
+++ js/functions-lec.js
@@ -1,5 +1,5 @@
 "use strict";
-
+(function(){
 //someFunctions(arguments)
 //Arguments are OPTIONAL
 
@@ -10,5 +10,54 @@
 // }
 
 
+// ================ !! Mini-exercises !!
+
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+
+function returnFive(){
+    return 5;
+}
+
+console.log(returnFive());
+
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+
+function isFive(num) {
+    return console.log(Boolean(Number(num) == 5))
+}
+isFive("5.000");
+// how many inputs should the function have?
+// 1
+// what are the data types for the inputs?
+// number or string
+// what is the data type of the output/return value?
+// boolean
 
 
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+
+function isShortWord(str){
+    return console.log(str.length < 5);
+}
+
+isShortWord("    ")
+
+// how many inputs?
+// what data type for inputs?
+// what is the return type?
+
+
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+
+function isSameLength(str1,str2){
+    return  console.log(str1.length == str2.length);
+}
+
+isShortWord(5, 5);
+
+// How many inputs?
+// What type of input?
+// What type of output?
+
+
+}())
