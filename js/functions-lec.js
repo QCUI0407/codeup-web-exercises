3018e30d46f276f4fe0cc600d8e2c169bcd1d4e7
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2022-10-06 10:04:23 -0500

    function file

---
+++ js/functions-lec.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//someFunctions(arguments)
+//Arguments are OPTIONAL
+
+
+//function myFunction(input) {
+//     var output = /* do something to calculate output */;
+//     return output;
+// }
+
+
+
+
