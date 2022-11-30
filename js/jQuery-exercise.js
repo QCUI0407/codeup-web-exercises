fc611e39d1f8d0b8757a6a351193cf49c12a26bf
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2022-11-30 15:55:01 -0600

    ajax exe

--- js/jQuery-exercise.js
+++ js/jQuery-exercise.js
@@ -24,34 +24,34 @@ $('li').hover(
 );
 //up:38 left:37 right:39 down:40 a:65 b:66 enter:13
 
-// $(function konami() {
-//     let input = "";
-//     let codes = "38373940656613";
-//     $('#konamiBox').keydown(function(e) {
-//         input += e.keyCode;
-//         if (input.indexOf(codes) !== -1) {
-//             input = "";
-//             alert("30")
-//         }
-//     });
-// })
-
 $(function konami() {
-    let input = [];
-    let codes = [38,37,39,40,65,66,13];
-    let count = 0;
+    let input = "";
+    let codes = "38373940656613";
     $('#konamiBox').keydown(function(e) {
-        input =input.push(e.keyCode);
-        count++
-        for(let i=0;i<codes.length;i++){
-            for(let j=0;j<input.length;j++){
-                if(codes[i]===input[i] && i===j){
-                alert('30');
-            }}
-
+        input += e.keyCode;
+        if (input.indexOf(codes) !== -1) {
+            input = "";
+            alert("30")
         }
     });
 })
+
+// $(function konami() {
+//     let input = [];
+//     let codes = [38,37,39,40,65,66,13];
+//     let count = 0;
+//     $('#konamiBox').keydown(function(e) {
+//         input =input.push(e.keyCode);
+//         count++
+//         for(let i=0;i<codes.length;i++){
+//             for(let j=0;j<input.length;j++){
+//                 if(codes[i]===input[i] && i===j){
+//                 alert('30');
+//             }}
+//
+//         }
+//     });
+// })
 
 
 
